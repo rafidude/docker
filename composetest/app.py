0997fbbf65f5ce6d303bd9b9ef6f81130ace6611
Rafi Dudekula <rafi.dudekula@gmail.com>
2017-05-20 16:16:15 -0500

    python flask with haproxy scaling app

--- composetest/app.py
+++ composetest/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from redis import Redis
+import os
+import socket
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
+host = socket.gethostname()
 
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+    return 'Hello from Docker! I have been seen {} times.\n My host name is {}.'.format(count, host)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
---
+++ proxyflask/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello from FLASK. My Hostname is: %s \n" % (socket.gethostname())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
